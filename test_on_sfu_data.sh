349605756745fec339838988f7cb5a63078fab3b
Charles <xuchi.int@gmail.com>
2023-02-28 18:22:47 -0800

    add train.yaml and detection shell code

---
+++ test_on_sfu_data.sh
@@ -0,0 +1,7 @@
+#
+#
+
+python detect.py --weights runs/train/exp10/weights/best.pt --source D:\sonar\2020-05-27_071000.mp4 --conf-thres 0.7 --view-img
+
+
+python detect.py --weights runs/train/exp10/weights/best.pt --source d:\sonar\caltech_2018-05-27_180004_1295_1895.mp4 --conf-thres 0.7 --view-img
